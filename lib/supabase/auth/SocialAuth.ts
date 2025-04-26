e542bbc6a0db93cb8be8aee2485f5bc3fb4589cd
vivek singh <vickydigari6154@gmail.com>
2025-04-26 12:08:37 +0530

    setup: redirect url setup for social auths

--- lib/supabase/auth/SocialAuth.ts
+++ lib/supabase/auth/SocialAuth.ts
@@ -5,7 +5,7 @@ export const GoogleAuth = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: 'http://127.0.0.1:3000/api/auth/socialCallback',
+          redirectTo: process.env.NODE_ENV === 'development'  ? 'http://127.0.0.1:3000/api/auth/socialCallback' : 'https://code-it-snowy.vercel.app/api/auth/socialCallback',
             queryParams: {
               access_type: 'offline',
               prompt: 'consent',
@@ -23,7 +23,7 @@ export const GithubAuth = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "github",
         options: {
-          redirectTo: 'http://127.0.0.1:3000/api/auth/socialCallback',
+          redirectTo: process.env.NODE_ENV === 'development'  ? 'http://127.0.0.1:3000/api/auth/socialCallback' : 'https://code-it-snowy.vercel.app/api/auth/socialCallback',
             queryParams: {
               access_type: 'offline',
               prompt: 'consent',
